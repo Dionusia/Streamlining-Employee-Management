368f17b8c38f2418c957f20450e45257ec5d218c
Dionusia Psirri <66883669+Dionusia@users.noreply.github.com>
2024-03-27 12:22:26 +0200

    path

--- intro/intro/src/main/java/com/example/intro/service/CompanyService.java
+++ intro/intro/src/main/java/com/example/intro/service/CompanyService.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:intro/src/main/java/com/example/intro/service/CompanyService.java
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -265,3 +266,66 @@ public class CompanyService {
 >>>>>>> parent of ee5891e (Revert "change path folder")
 =======
 >>>>>>> parent of ba7e421 (Revert "change path")
+=======
+package com.example.intro.service;
+
+import com.example.intro.dto.CompanyDTO;
+import com.example.intro.entity.Company;
+import com.example.intro.entity.Employee;
+import com.example.intro.repository.CompanyRepository;
+import com.example.intro.repository.EmployeeRepository;
+import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class CompanyService {
+
+    @Autowired
+    private CompanyRepository companyRepository;
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public CompanyDTO saveCompany(CompanyDTO companyDTO){
+        companyRepository.save(modelMapper.map(companyDTO, Company.class));
+        return companyDTO;
+    }
+
+    public List<CompanyDTO> getAllCompanies(){
+        List<Company> companyList = companyRepository.findAll();
+        return modelMapper.map(companyList, new TypeToken<List<CompanyDTO>>(){}.getType());
+    }
+
+    public CompanyDTO updateCompany(CompanyDTO companyDTO){
+        companyRepository.save(modelMapper.map(companyDTO,Company.class));
+        return companyDTO;
+    }
+
+    public boolean deleteCompany(CompanyDTO companyDTO){
+        companyRepository.delete(modelMapper.map(companyDTO, Company.class));
+        return true;
+    }
+
+    public void deleteAllCompanies() {
+        companyRepository.deleteAll();
+    }
+
+    public Double getTotalSalariesForCompany(Long companyId) {
+        List<Employee> employees = employeeRepository.findByCompanyId(companyId);
+
+        double totalSalaries;
+        totalSalaries = employees.stream()
+                .mapToDouble(employee -> employee.getSalary().doubleValue())
+                .sum();
+
+        return totalSalaries;
+    }
+}
+>>>>>>> parent of 9f464f9 (change path folder):intro/intro/src/main/java/com/example/intro/service/CompanyService.java
--- intro/intro/src/main/java/com/example/intro/service/ProductService.java
+++ intro/intro/src/main/java/com/example/intro/service/ProductService.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:intro/src/main/java/com/example/intro/service/ProductService.java
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -232,3 +233,55 @@ public class ProductService {
 >>>>>>> parent of ee5891e (Revert "change path folder")
 =======
 >>>>>>> parent of ba7e421 (Revert "change path")
+=======
+package com.example.intro.service;
+
+import com.example.intro.dto.EmployeeDTO;
+import com.example.intro.dto.EmployeeProductDTO;
+import com.example.intro.dto.ProductDTO;
+import com.example.intro.entity.Employee;
+import com.example.intro.entity.EmployeeProduct;
+import com.example.intro.entity.Product;
+import com.example.intro.repository.ProductRepository;
+import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@Transactional
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public ProductDTO saveProduct(ProductDTO productDTO) {
+        productRepository.save(modelMapper.map(productDTO, Product.class));
+        return productDTO;
+    }
+
+    public List<ProductDTO> getAllProducts() {
+        List<Product> productList = productRepository.findAll();
+        return modelMapper.map(productList, new TypeToken<List<ProductDTO>>() {
+        }.getType());
+    }
+
+    public ProductDTO updateProduct(ProductDTO productDTO) {
+        productRepository.save(modelMapper.map(productDTO, Product.class));
+        return productDTO;
+    }
+
+    public boolean deleteProduct(ProductDTO productDTO) {
+        productRepository.delete(modelMapper.map(productDTO, Product.class));
+        return true;
+    }
+}
+>>>>>>> parent of 9f464f9 (change path folder):intro/intro/src/main/java/com/example/intro/service/ProductService.java
